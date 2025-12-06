26d2dbbe55e47237f5c38080ad6ffac515f06356
pallavi2706 <pallavis2706@gmail.com>
2025-12-06 23:40:56 +0530

    Update app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -4,7 +4,7 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 # Download and load the model
-model_path = hf_hub_download(repo_id="pal27/best_tourism_model_v1", filename="best_tourism_model_v1.joblib")
+model_path = hf_hub_download(repo_id="pal27/tourism_model", filename="best_tourism_model_v1.joblib")
 model = joblib.load(model_path)
 
 # Streamlit UI for Machine Failure Prediction
